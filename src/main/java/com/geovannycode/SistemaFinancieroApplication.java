2d616b11ee71a0493d69e7a6e63a6d374671f3ff
Geovanny Mendoza <me@geovannycode.com>
2025-09-13 22:14:14 -0500

    Inicializar estructura del proyecto con configuración base y aplicación principal

---
+++ src/main/java/com/geovannycode/SistemaFinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.geovannycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFinancieroApplication.class, args);
+	}
+
+}
